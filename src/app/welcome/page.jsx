ef7510ff8743d52c8f88d2428b6d131e469f52b5
MAGDALENA KAPUSCINSKA <MADZIAA82@HOTMAIL.COM>
2024-10-16 15:39:54 +0100

    adding screenshots

--- src/app/welcome/page.jsx
+++ src/app/welcome/page.jsx
@@ -1,8 +1,27 @@
-import Link from "next/link"
+import Link from "next/link";
+import Image from "next/image";
 
 export default function welcome() {
     return (
-        <h1>Week 10 Research Project</h1>
-
+        <div className="flex flex-col p-8 text-3x">
+            <h1 className="">Week 10 Research Project</h1>
+            <div className="flex justify-center ">
+            <Image 
+                src="/images/figma.png"
+                width={500}
+                height={500}
+                alt="figma screenshot"
+                />
+            <Image 
+                src="/images/figma.png"
+                width={400}
+                height={400}
+                alt="figma screenshot"
+                />
+            </div>
+           
+            <Link className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors absolute bottom-5 right-5 text-2xl" href="/description">Next Page</Link>  
+        </div>
     )
 }
+
